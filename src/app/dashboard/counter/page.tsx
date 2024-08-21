36b9b2db3eacaa3411374702619ec36200b542fa
jojupacr5 <joseph.paucar@gmail.com>
2024-08-21 16:15:40 -0500

    Se agregaron iconos y se crearon partes de una pagina de parte del cliente y parte del servidor

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,17 @@
+import { CartCounter } from "@/app/shopping-cart/components";
+
+export const metadata = {
+ title: 'Counter Page',
+ description: 'Un simple contador',
+};
+
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      
+      <CartCounter />
     </div>
   )
 }
