3eda7b3fd264a199e7d4ffa1139baaa46f6bf548
jojupacr5 <joseph.paucar@gmail.com>
2024-08-21 23:13:17 -0500

    Trabajando con fetch hacia la pokeapi

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,18 @@
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then( res => res.json());
+
+  return data;
+}
+
+
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
+  return (
+    <div>
+      { JSON.stringify(pokemons) }
+    </div>
+  )
+}
